2737fd5f642a509d087569fa8f84046897e14b55
mmgl <muhammedmustafagull@gmail.com>
2020-12-24 13:20:23 +0300

    masterpage..

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('home', function () {
+    return view('home.index');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
